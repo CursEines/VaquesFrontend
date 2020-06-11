b39d99864f74040de04064b1957765b22d9e0e38
Xavier Sala <utrescu@gmail.com>
2020-06-11 16:23:40 +0200

    First version

---
+++ Pages/VaquesData.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using vacawasm.Model;
+
+namespace vacawasm
+{
+
+    public class VaquesData
+    {
+        protected List<Vaca> Camp;
+        protected string imatgeCamp = "/img/camp.png";
+        protected List<Vaca> Camio;
+        protected string imatgeCamio = "/img/camio.png";
+
+        protected string imatgeCiutat = "/img/ciutat.png";
+        protected List<Vaca> Ciutat;
+
+
+        public void move(Vaca vaca)
+        {
+
+        }
+        protected double Diners;
+    }
+}
