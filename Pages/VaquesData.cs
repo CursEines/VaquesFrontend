eadeb517e18cf55a9273a6c58c3963c5ea5c15ae
Xavier Sala <utrescu@gmail.com>
2020-06-12 08:22:35 +0200

    Add more data

--- Pages/VaquesData.cs
+++ Pages/VaquesData.cs
@@ -20,20 +20,46 @@ namespace vacawasm
 
         protected override async Task OnInitializedAsync()
         {
-            // Recuperar les dades
+            // Recuperar les dades de http://localhost:4567/start/6
+            //  Camp =  http://localhost:4567/camp
+            //  Camio = http://localhost:4567/camio
+            //  Ciutat = http://localhost:4567/ciutat
             Camp.Add(new Vaca()
             {
                 Nom = "Pepa",
                 Pes = 350.3,
                 Raça = new Raça() { Nom = "Holdstein", LitresPerKg = 4.3 }
             });
+
+            Camp.Add(new Vaca()
+            {
+                Nom = "Rufa",
+                Pes = 378.0,
+                Raça = new Raça() { Nom = "Ayshire", LitresPerKg = 3.3 }
+            });
+
+            Camp.Add(new Vaca()
+            {
+                Nom = "Flor",
+                Pes = 450.9,
+                Raça = new Raça() { Nom = "Jersey", LitresPerKg = 3.9 }
+            });
+
+            Camp.Add(new Vaca()
+            {
+                Nom = "Toñi",
+                Pes = 550.7,
+                Raça = new Raça() { Nom = "Holdstein", LitresPerKg = 4.3 }
+            });
+
+            Camp.Add(new Vaca()
+            {
+                Nom = "Marisol",
+                Pes = 450.3,
+                Raça = new Raça() { Nom = "Simental", LitresPerKg = 3.1 }
+            });
         }
 
-        public void move(Vaca vaca)
-        {
-
-
-        }
         protected double Diners;
     }
 }
